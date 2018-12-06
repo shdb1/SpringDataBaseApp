77ac9f4ffbab535f33d8b0886ca52e96a8b22521
moshadab <moshadab@lm-maa-22017556>
2018-12-06 08:41:15 +0530

    added queries file

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,7 @@
+create table students (name text,age int,address text)
+
+insert into students values('Ram',20,'Chennai')
+insert into students values('Shyam',25,'Bengal')
+insert into students values('John',21,'NY')
+
+select * from students
